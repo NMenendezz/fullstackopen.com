73e7a05b49e7425653bf6e4a667e882f8cc9e9b7
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-06-21 21:41:53 +0200

    Update informacion_del_curso 2.1

---
+++ part2/informacion_del_curso/src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+/* const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
+}; */
+
+const courses = [
+  {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  },
+];
+
+ReactDOM.render(<App courses={courses} />, document.getElementById("root"));
