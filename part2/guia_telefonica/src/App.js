623411045e19ad77c882f7fc8d37ac66f1da24b8
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-06-22 10:33:09 +0200

    Update guia_telefonica 2.8

--- part2/guia_telefonica/src/App.js
+++ part2/guia_telefonica/src/App.js
@@ -2,25 +2,36 @@ import React, { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
-    if (persons.filter((person) => person.name === personObject.name).length > 0) {
+    if (
+      persons.filter((person) => person.name === personObject.name).length > 0
+    ) {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -29,6 +40,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
