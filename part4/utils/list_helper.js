e2401c4fdd5923dc33a0067a9af3c140579c4154
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-07-08 11:06:35 +0200

    Update lista_de_blogs 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,12 +1,18 @@
 const dummy = () => 1;
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, item) =>
-    sum + item.likes;
-  return blogs.reduce(reducer, 0);
+  const reducer = (sum, item) => sum + item.likes;
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  const blog = blogs[likes.indexOf(Math.max(...likes))];
+  return blog;
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
